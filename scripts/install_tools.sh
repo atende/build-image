497b4f2a0aaad0f403af6f87d4a4397207485d67
Giovanni Silva <giovanni@atende.info>
2017-05-13 20:48:13 -0300

    update nodejs

--- scripts/install_tools.sh
+++ scripts/install_tools.sh
@@ -21,7 +21,7 @@ apt-get install -y postgresql-client
 
 
 # Installing NodeJS
-curl -sL https://deb.nodesource.com/setup_5.x | bash -
+curl -sL https://deb.nodesource.com/setup_6.x | bash -
 apt-get install -y nodejs
 apt-get install -y build-essential
 
@@ -30,6 +30,7 @@ npm install -g gulp
 npm install -g bower
 npm install -g jspm
 npm install -g typings
+npm install -g yarn
 # Installing Google Dart
 #add-apt-repository -y ppa:hachre/dart
 #apt-get update -q
